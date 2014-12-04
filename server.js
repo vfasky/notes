c91deaf644ba2bdb7b8bc7e37f18762fcbff7091
vfasky <vfasky@gmail.com>
2014-12-04 23:09:09 +0800

    修改启动文件

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-
+/**
+ * Application 启动文件
+ */
 "use strict";
 
 var koa = require('koa');
@@ -6,20 +8,36 @@ var koa = require('koa');
 var session = require('koa-generic-session');
 var memcachedStore = require('koa-memcached');
 var config = require('./config');
-
-console.log(config);
+var template = require('./app/lib/template');
 
 var app = module.exports = koa();
 
-app.keys = ['keys', 'keykeys'];
+/**
+ * 监听端口
+ * @type {Int}
+ */
+var port = process.env.PORT || 3000;
+
+/**
+ * 设置一个签名 Cookie 的密钥
+ * @type {Array}
+ */
+app.keys = config.keys;
+
+/**
+ * 定义 session store
+ */
 app.use(session({
   store: memcachedStore()
 }));
 
-app.use(function *(){
-  this.body = {
-    test: 'ok?'
-  };
+/**
+ * 加载模板引擎
+ */
+template(app, {
+	templatePath: config.templatePath
 });
 
-if (!module.parent) app.listen(3000);
+if (!module.parent) {
+	app.listen(port);
+}

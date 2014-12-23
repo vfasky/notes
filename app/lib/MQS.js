35f7de394e6c63c9bf13f23ed4f310ca7c195e93
vfasky <vfasky@gmail.com>
2014-12-23 23:07:19 +0800

    添加MQS

---
+++ app/lib/MQS.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * 阿里云的MQS
+ * @author vfasky <vfasky@gmail.com>
+ */	
+var config = require('../../config');
+var MQSClient = require('aliyun_mqs');
+
+
+var client = new MQSClient(config.MQS);
+
+var _emailQueue = 'note-email';
+var _indexQueue = 'note-index';
+
+var nameMap = {
+	'email': _emailQueue,
+	'index': _indexQueue
+};
+
+module.exports = client;
+
+module.exports.send = function(type, msg){
+	return client.message.send(nameMap[type], {
+        msg: JSON.stringify(msg)
+    });
+};
+
+module.exports.get = function(type, wait){
+	return client.message.get(nameMap[type], wait);
+};

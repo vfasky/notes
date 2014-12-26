a03a375e6ff074dfc955deafebcc946138538220
vfasky <vfasky@gmail.com>
2014-12-26 18:02:04 +0800

    优化任务执行

--- task.js
+++ task.js
@@ -2,7 +2,7 @@
 /**
  * 执行任务
  * @author vfasky <vfasky@gmail.com>
- */	
+ */
 var config = require('./config');
 var validator = require('validator');
 var MQS = require('./app/lib/MQS');
@@ -14,80 +14,91 @@ var model = require('./app/model');
 var wait = 2;
 
 /**
- * 执行完，休息 2 秒再执行
+ * 执行完，休息 1 秒再执行
  * @type {Number}
  */
-var sleep = 2;
+var sleep = 1;
 
 /**
  * 错误记录
  * @author vfasky <vfasky@gmail.com>
  * @date   2014-12-26
- * @param  {String} type 
+ * @param  {String} type
  * @param  {[type]} err [description]
  * @return {[type]} [description]
  */
-var log = function(type, err){
-	//TODO 记录
-	console.log(type + ': ' + err);	
+var log = function(type, err) {
+    //TODO 记录
+    console.log(type + ': ' + err);
 };
 
 /**
- * 取笔记索引任务
- * @author vfasky <vfasky@gmail.com>
- * @date   2014-12-26
- * @param  {Function} callback 完成后回调
+ * 取任务
  */
-var getNoteIndexTask = function(callback){
-	var next = function(){
-		setTimeout(callback, sleep * 1000);
-	};
+var getTask = function(type, callback) {
+    var next = function() {
+        setTimeout(callback, sleep * 1000);
+    };
 
-	MQS.message.get(MQS.index, wait).then(function(data){
-		//console.log(data.MessageBody);
-		var id = data.MessageBody;
-		if(validator.isMongoId(id)){
-			model.Note.findOne({
-				_id: id
-			}, function(note, err){
-				if(err){
-					log('index', err);
-					return next();
-				}
-				if(null === note){
-					//console.log('note is Null');
-					MQS.message.remove(MQS.index, data.ReceiptHandle)
-					   .then(next, function(err){
-							log('index', err);	
-					   });
-				}
-				else{
-					note.buildIndex()(next);
-				}
-			});
-		}
-		else{
-			MQS.message.remove(MQS.index, data.ReceiptHandle)
-			   .then(next, function(err){
-					log('index', err);	
-			   });
-		}
-		
-	}, function(err){
-		if('Message not exist.' !== err){
-			log('index', err);
-		}
-		next();
-	});
+    var remove = function(receiptHandle) {
+        MQS.message.remove(type, receiptHandle)
+            .then(next, function(err) {
+                log(type, err);
+                next();
+            });
+    };
+
+    var run = {};
+
+    //创建索引任务
+    run[MQS.index] = function(data){
+    	var id = data.MessageBody;
+        if (validator.isMongoId(id)) {
+            model.Note.findOne({
+	            _id: id
+	        }, function(note, err) {
+	            if (err) {
+	                log(type, err);
+	                return next();
+	            }
+	            if (null === note) {
+	                remove(data.ReceiptHandle);
+	            } else {
+	                note.buildIndex()(function(err) {
+	                    if (err) {
+	                        log(type, err);
+	                        return next();
+	                    }
+	                    remove(data.ReceiptHandle);
+	                });
+	            }
+	        });	
+        } else {
+            remove(data.ReceiptHandle);
+        }
+    };
+
+    MQS.message.get(type, wait).then(function(data) {
+
+        run[type](data);
+
+    }, function(err) {
+        if ('Message not exist.' !== err) {
+            log('index', err);
+        }
+        console.log('not ' + type + ' task');
+        next();
+    });
 };
 
-var noteIndexLoop = function(){
-	getNoteIndexTask(noteIndexLoop);
+var taskLoop = function() {
+    getTask(MQS.email, function() {
+        getTask(MQS.index, taskLoop);
+    });
 };
 
-if(config.MQS.accessKeyId !== 'you accessKeyId'){
-	noteIndexLoop();
-}
-else{
-	throw Error ('MQS config error');
+if (config.MQS.accessKeyId !== 'you accessKeyId') {
+    taskLoop();
+} else {
+    throw Error('MQS config error');
 }
